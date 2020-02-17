6a0affb7c0581005ef8a7c9d17d884c12327ea4c
Ed Baker <edwbaker@gmail.com>
2020-02-17 23:24:41 +0000

    Paging

--- index.php
+++ index.php
@@ -1,45 +1,92 @@
 <?php
+global $_POST;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Connect to audioblast SQL server
 include('database.php');
+
+//Have we been sent a query array?
+if (isset($_GET["query"])) {
+  $qa = urldecode($_GET["query"]);
+  $qa =  json_decode($qa, true);
+} else {
+  $qa = array();
+}
+
+if (!isset($qa["init"])) {
+  $qa["init"] = true;
+}
+
+
+//Array of plugins to include
 $plugins = array(
   "limits" => "limits.php"
 );
 
+//Get info about plugins that is needed to process them
 foreach ($plugins as $name => $path) {
   include("plugins/".$path);
   $info = $name."_info";
   $plugins[$name] = $info();
+  if ($qa["init"] == true) {
+    $init = $name."_init";
+    $qa = $init($qa);
+  }
   if (isset($plugins[$name]["filter html"])) {
     $filt = $plugins[$name]["filter html"];
-    $response["filter_html"][] = $filt();
+    $qa["filter_html"][] = $filt($qa);
   }
 }
 
-$query = "SELECT * FROM `audioblast-traits`.`taxa`";
+if (isset($qa["update filter"])) {
+  $plugin = $qa["update filter"]["plugin"]."_update_filter";
+  $qa = $plugin($qa, $qa["update filter"]["activity"], $qa["update filter"]["value"]);
+  unset($qa["update filter"]);
+}
 
+//Base query that is modifed by the plugins
+$query = "SELECT SQL_CALC_FOUND_ROWS * FROM `audioblast-traits`.`taxa`";
+
+//Only allow one plugin to set a limit
 $num_limits = 0;
 foreach ($plugins as $name => $data) {
   if (isset($data["limits"])) {
     if ($num_limits == 0) {
       $func = $name."_limits";
-      $query = $func($query);
+      $query = $func($query, $qa);
     } else {
-      $exec_notes[] = "SQL LIMIT suggested by plugin $name was not used as LIMIT had already been set.";
+      $qa["exec_notes"][] = "SQL LIMIT suggested by plugin $name was not used as LIMIT had already been set.";
     }
     $num_limits++;
   }
 }
 
-$result = mysqli_query($db,$query);
+//Execute modified query
+$query .= "; SELECT FOUND_ROWS();";
+$results = mysqli_multi_query($db,$query);
 
 $taxa = array();
-
+$result = mysqli_store_result($db);
 while($row = mysqli_fetch_assoc($result)) {
   $taxa[] = $row;
 }
 
-$response["taxa"] = $taxa;
+mysqli_next_result($db);
+$counts = mysqli_store_result($db);
+$count = mysqli_fetch_assoc($counts);
 
-echo json_encode($response);
+//Process plugins that need the result count
+foreach ($plugins as $name => $path) {
+  if (isset($plugins[$name]["result count"])) {
+    $filt = $plugins[$name]["result count"];
+    $qa = $filt($qa, $count["FOUND_ROWS()"]);
+  }
+}
+
+$qa["taxa"] = $taxa;
+$qa["init"] = false;
+echo json_encode($qa);
 
 exit;
-
--- plugins/limits.php
+++ plugins/limits.php
@@ -1,24 +1,69 @@
 <?php
+global $qa;
+
 function limits_info() {
   return(array(
     "name" => "Limit results",
     "desc" => "Provies the results pager",
     "auth" => "edwbaker@gmail.com",
     "filter html" => "limits_html",
-    "limits" => "limits_limits"
+    "limits" => "limits_limits",
+    "result count" => "limits_result_count"
   ));
 }
 
-function limits_html() {
+function limits_result_count($qa, $count) {
+  $page = $qa["limits"]["page"];
+  $page_count = ceil($count / $qa["limits"]["value"]);
+
+  $content = "<div id='limits-row-count'>Page $page of $page_count</div>";
+
+  $content .= "<div id='pager'>";
+  for ($i =1; $i <=$page_count; $i++) {
+    $content .= '<a onclick="updateFilter(\'limits\', \'page\', '.$i.');">'.$i.'</a>';
+  }
+  $content .= "</div>";
+
+  $qa["results_head_html"][] = $content;
+  return($qa);
+}
+
+function limits_update_filter($qa, $activity, $value) {
+  if ($activity == "value") {
+    $qa["limits"]["value"] = $value;
+  }
+  if ($activity == "page") {
+    $qa["limits"]["page"] = $value;
+  }
+ return($qa);
+}
+
+function limits_init($qa) {
+  $qa["limits"] = array(
+    "value" => 25,
+    "page" => 1
+  );
+  return($qa);
+}
+
+function limits_html($qa) {
   $output  = '<div id="filter_limits" class="filter">';
   $output .= '<h2>Show results</h2>';
-  $output .= '<input type="radio" id="limits25" name="limits" value="25"><label for="25">25</label><br>';
-  $output .= '<input type="radio" id="limits50" name="limits" value="50"><label for="50">50</label><br>';
+  $output .= '<input type="radio" id="limits25" name="limits" value="25" ';
+  if ($qa["limits"]["value"] == 25) {$output .= "checked ";}
+  $output .= 'onclick="updateFilter(\'limits\', \'value\', 25);"><label for="25">25</label><br>';
+  $output .= '<input type="radio" id="limits50" name="limits" value="50" ';
+  if ($qa["limits"]["value"] == 50) {$output .= "checked ";}
+  $output .= 'onclick="updateFilter(\'limits\', \'value\', 50);"><label for="50">50</label><br>';
   $output .= '</div>';
+
+
+  $output.= "<script>alert('Hi');</script>";
   return($output);
 }
 
-function limits_limits($sql) {
-  $sql .= " LIMIT 25";
+function limits_limits($sql, $qa) {
+  $offset =  ($qa["limits"]["page"] - 1) * $qa["limits"]["value"];
+  $sql .= " LIMIT ".$offset.", ".$qa["limits"]["value"];
   return($sql);
 }
