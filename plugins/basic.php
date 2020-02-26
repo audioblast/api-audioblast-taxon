bdab8c44af8f6d8dfa11b5d676966ec8db1d81e2
Ed Baker <edwbaker@gmail.com>
2020-02-26 16:51:01 +0000

    Limit to non-silent species now db speed is better

--- plugins/basic.php
+++ plugins/basic.php
@@ -60,7 +60,7 @@ function basic_init($qa) {
   $qa["basic"] = array(
     "traits" => "checked=checked",
     "speciesGroup" => "checked=checked",
-    "silent" => "",
+    "silent" => "checked=checked",
     "recordings" => ""
   );
   return($qa);
